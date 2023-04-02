39d1f4fd94ab7713d0f09660fb453aa72be6cfa1
vitalik12345678 <58762669+vitalik12345678@users.noreply.github.com>
2023-04-02 20:45:45 +0300

    Revert "Test"

--- src/main/java/com/tiva/event/repository/GradePostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiva.event.repository;
-
-import com.tiva.event.model.GradePost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GradePostRepository extends JpaRepository<GradePost, Long> {
-}
