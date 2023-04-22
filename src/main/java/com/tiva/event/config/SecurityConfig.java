1aecb307bb115e8129891362ab037bdada7d6cb8
Mikhalov.Ivan <112470475+larbadge@users.noreply.github.com>
2023-04-22 16:18:09 +0300

    added login authentication endpoint, restructured controller and service classes

--- src/main/java/com/tiva/event/config/SecurityConfig.java
+++ src/main/java/com/tiva/event/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import com.tiva.event.jwt.JwtAthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -24,13 +29,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/signup").permitAll()
+                        .requestMatchers("/auth/signup", "/auth/login").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAthFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(Customizer.withDefaults())
                 .userDetailsService(userDetailsService)
                 .csrf().disable()
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() {
+        return new ProviderManager(Collections.singletonList(authenticationProvider()));
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
     }
 
     @Bean
--- src/main/java/com/tiva/event/service/UserService.java
+++ src/main/java/com/tiva/event/service/UserService.java
@@ -1,24 +1,38 @@
 package com.tiva.event.service;
 
+import com.tiva.event.dto.UserDTO;
+import com.tiva.event.mapper.UserMapper;
 import com.tiva.event.model.User;
 import com.tiva.event.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
 
-    public User create(User user) {
-        return userRepository.save(user);
+    public UserDTO create(UserDTO userDTO) {
+        User user = userMapper.dtoToModel(userDTO);
+        user.setPassword(new BCryptPasswordEncoder().encode(userDTO.getPassword()));
+        user = userRepository.save(user);
+
+        return userMapper.modelToDto(user);
+    }
+
+    public void delete(Long id) {
+        UserDTO userDTO = userMapper.modelToDto(userRepository.findById(id).orElseThrow());
+        userRepository.deleteById(userDTO.getUserId());
     }
 
     public User getUserByPhone(String phone) {
         return userRepository.findByPhone(phone)
                 .orElseThrow(() -> new UsernameNotFoundException("Do not found user with phone" + phone));
     }
+
 
 }
